45b2a9dff93f9eead01a69ae5a1ce0674d86bfbf
Juvenal J. Duarte <juvenaldrt@gmail.com>
2016-09-11 18:40:01 -0300

    The guardian is back and memory management improved.

--- crawler/RetrieveArticles.PROD.py
+++ crawler/RetrieveArticles.PROD.py
@@ -14,16 +14,19 @@ logging.basicConfig(level=logging.CRITICAL)
 estadaoRegex = re.compile('.*(economia.estadao|politica.estadao).*', re.IGNORECASE)
 estadao = NPSpyderBR('estadao.com.br','http://www.estadao.com.br/', estadaoRegex)
 estadao.run()
+gc.collect()
 
 # Build UOL
 uolRegex = re.compile('.*(economia.uol|noticias.uol).*', re.IGNORECASE)
 uol = NPSpyderBR('uol.com.br','http://www.uol.com.br/', uolRegex)
 uol.run()
+gc.collect()
 
 # Build ADFVN
 adfvnRegex = re.compile('.*', re.IGNORECASE)
 adfvn = NPSpyderBR('br.advfn.com','http://br.advfn.com/', adfvnRegex)
 adfvn.run()
+gc.collect()
 
 
 # International
@@ -33,13 +36,16 @@ adfvn.run()
 bloombergRegex = re.compile('.*(articles).*', re.IGNORECASE)
 bloomberg = NPSpyder('bloomberg.com','http://www.bloomberg.com/', bloombergRegex)
 bloomberg.run()
+gc.collect()
 
 # Build Reuters
 reutersRegex = re.compile('.*(article).*', re.IGNORECASE)
 reuters = NPSpyder('reuters.com','http://www.reuters.com/', reutersRegex)
 reuters.run()
+gc.collect()
 
 # Build The Guardian
-#theGuardianRegex = re.compile('.*', re.IGNORECASE)
-#theGuardian = NPSpyder('theguardian.com','https://www.theguardian.com/', theGuardianRegex)
-#theGuardian.run()
+theGuardianRegex = re.compile('.*', re.IGNORECASE)
+theGuardian = NPSpyder('theguardian.com','https://www.theguardian.com/', theGuardianRegex)
+theGuardian.run()
+gc.collect()
