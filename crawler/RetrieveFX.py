ab0b5ffaa297c3d435359a62d147447bd60f6afc
jduarte <jduarte@gmail.com>
2021-02-10 10:38:03 -0200

    latest updates

--- crawler/RetrieveFX.py
+++ crawler/RetrieveFX.py
@@ -8,14 +8,27 @@ with open("../cfg/forex.cfg") as f:
 
 for s in s_list:
    s = s.rstrip()
+   s2save = s.split("=")[0]
+
+   try:
+      df_previous = pd.read_csv("/home/jduarte/series/YAHOO_"+ s2save +".csv")
+   except:
+      print("*WARN - No previous history was found for %s. Saving serie from zero." %(s))
+      df_previous = pd.DataFrame()
+
 
    try:
       dfs = web.DataReader(s, data_source="yahoo", start="01-01-2016")
 
-      print("*INFO - Captured %s." %(s))
+      n_previous = len(df_previous)
+      n_actual   = len(dfs)
 
-      s2save = s.split("=")[0]
-      dfs.to_csv("/home/jduarte/series/YAHOO_"+ s2save +".csv")
+      if (n_actual > n_previous):
+         print("*INFO - Captured %s. %d points added to previous serie." %(s, (n_actual - n_previous)))
+         dfs.to_csv("/home/jduarte/series/YAHOO_"+ s2save +".csv")
+      else:
+         print("*WARN - New serie for %s has less data points: Previous=%d/ Now=%d. File won't be updated, check data source for more info." %(s, n_previous, n_actual))
+
 
    except:
       print("*WARN - Could not capture %s." %(s))
--- crawler/RetrievePrices.py
+++ crawler/RetrievePrices.py
@@ -11,10 +11,23 @@ for s in s_list:
    s = s.rstrip()
 
    try:
+      df_previous = pd.read_csv("/home/jduarte/series/YAHOO_"+ s +".csv")
+   except:
+      print("*WARN - No previous history was found for %s. Saving serie from zero." %(s))
+      df_previous = pd.DataFrame()
+
+
+   try:
       dfs = web.DataReader(s, data_source="yahoo", start="01-01-2016")
 
-      print("*INFO - Captured %s." %(s))
-      dfs.to_csv("/home/jduarte/series/YAHOO_"+ s +".csv")
+      n_previous = len(df_previous)
+      n_actual   = len(dfs)
+
+      if (n_actual > n_previous):
+         print("*INFO - Captured %s. %d points added to previous serie." %(s, (n_actual - n_previous)))
+         dfs.to_csv("/home/jduarte/series/YAHOO_"+ s +".csv")
+      else:
+         print("*WARN - New serie for %s has less data points: Previous=%d/ Now=%d. File won't be updated, check data source for more info." %(s, n_previous, n_actual))
 
    except:
       print("*WARN - Could not capture %s." %(s))
